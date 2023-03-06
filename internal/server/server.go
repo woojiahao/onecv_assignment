4dace05091d9c8666b20623cbd0feafe88e0306a
Woo Jia Hao <woojiahao1234@gmail.com>
2023-03-06 14:11:07 +0800

    Start basic server

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,6 +1,10 @@
 package server
 
-import "github.com/woojiahao/onecv_assignment/internal/database"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/woojiahao/onecv_assignment/internal/database"
+)
 
 func Start(db *database.Database) {
+	gin.Default().Run()
 }
