8341076a4cb5cb21448d1b098d3b1bd8da3c2769
Woo Jia Hao <woojiahao1234@gmail.com>
2023-03-06 14:10:00 +0800

    Implement register students

--- internal/database/errors.go
+++ internal/database/errors.go
@@ -5,4 +5,5 @@ import "errors"
 var (
 	ConflictingTeachersEntry = errors.New("conflicting teacher with same email")
 	ConflictingStudentsEntry = errors.New("conflicting student with same email")
+	DatabaseError            = errors.New("internal database error occurred")
 )
--- internal/database/teacher.go
+++ internal/database/teacher.go
@@ -18,3 +18,22 @@ func (db *Database) CreateTeacher(email string) (Teacher, error) {
 
 	return createdTeacher, nil
 }
+
+func (db *Database) RegisterStudents(teacherEmail string, studentEmails []string) error {
+	var parameters []string
+	var placeholders []string
+	for _, studentEmail := range studentEmails {
+		parameters = append(parameters, []string{teacherEmail, studentEmail}...)
+		placeholders = append(placeholders, fmt.Sprintf("(?, ?)"))
+	}
+	query := fmt.Sprintf(
+		`INSERT INTO TeacherStudents VALUES %s ON CONFLICT DO NOTHING;`,
+		strings.Join(placeholders, ", "),
+	)
+	_, err := db.Database.ExecContext(context.TODO(), query, parameters)
+	if err != nil {
+		return DatabaseError
+	}
+
+	return nil
+}
