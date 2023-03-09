29f1f84d0e9e3b68fe4886f82647dfc0493a7349
Woo Jia Hao <woojiahao1234@gmail.com>
2023-03-09 12:19:19 +0800

    Add initial data for database

--- sql/init.sql
+++ sql/init.sql
@@ -15,3 +15,24 @@ CREATE TABLE TeacherStudents
     student_email VARCHAR(256) NOT NULL REFERENCES Students,
     PRIMARY KEY (teacher_email, student_email)
 );
+
+INSERT INTO Teachers
+VALUES ('teacherken@gmail.com'),
+       ('teacherjoe@gmail.com');
+INSERT INTO Students (email)
+VALUES ('studentjon@gmail.com'),
+       ('studenthon@gmail.com'),
+       ('commonstudent1@gmail.com'),
+       ('commonstudent2@gmail.com'),
+       ('student_only_under_teacher_ken@gmail.com'),
+       ('studentagnes@gmail.com'),
+       ('studentbob@gmail.com'),
+       ('studentmary@gmail.com'),
+       ('studentmiche@gmail.com');
+INSERT INTO TeacherStudents
+VALUES ('teacherken@gmail.com', 'studentbob@gmail.com'),
+       ('teacherken@gmail.com', 'commonstudent1@gmail.com'),
+       ('teacherken@gmail.com', 'commonstudent2@gmail.com'),
+       ('teacherken@gmail.com', 'student_only_under_teacher_ken@gmail.com'),
+       ('teacherjoe@gmail.com', 'commonstudent1@gmail.com'),
+       ('teacherjoe@gmail.com', 'commonstudent2@gmail.com');
